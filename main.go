9b222f9b5d288fae13ca1d865ca0e184f9f1d194
Evan Lin <evan.if.lin@linecorp.com>
2023-04-24 15:46:30 +0800

    first level provide by openai

--- main.go
+++ main.go
@@ -23,23 +23,34 @@ type Stats struct {
 	LUC int
 }
 
+func NewPlayer(name string, stats Stats) *Player {
+	return &Player{
+		Name:  name,
+		Stats: stats,
+	}
+}
+
 func main() {
-	// 讀取兩個圖片檔案
-	img1 := loadImage("image1.png")
-	img2 := loadImage("image2.png")
+	// 讀取兩個圖片檔案，解析成人物資料
+	player1Data, err := readImage("player1.png")
+	if err != nil {
+		fmt.Println("Error reading player1.png:", err)
+		return
+	}
+	player2Data, err := readImage("player2.png")
+	if err != nil {
+		fmt.Println("Error reading player2.png:", err)
+		return
+	}
 
-	// 計算初五種數值
-	stats1 := calculateStats(img1)
-	stats2 := calculateStats(img2)
+	// 根據解析出來的資料，創建兩個人物
+	player1 := NewPlayer(player1Data.Name, player1Data.Stats)
+	player2 := NewPlayer(player2Data.Name, player2Data.Stats)
 
-	// 將初五種數值作為兩個玩家的屬性
-	player1 := Player{Name: "Player 1", Stats: stats1}
-	player2 := Player{Name: "Player 2", Stats: stats2}
-
-	// 開始對戰
+	// 讓這兩個人物進行對戰
 	winner := fight(player1, player2)
 
-	// 輸出勝利者
+	// 印出勝利者的名字
 	fmt.Printf("%s wins!\n", winner.Name)
 }
 
@@ -91,12 +102,52 @@ func calculateStats(img image.Image) Stats {
 	return stats
 }
 
-func fight(player1 Player, player2 Player) Player {
+func readImage(filePath string) (*Player, error) {
+	// 開啟圖片檔案
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	// 讀取圖片檔案
+	img, _, err := image.Decode(file)
+	if err != nil {
+		return nil, err
+	}
+
+	// 創建一個新的 CharacterData 物件
+	data := &Player{}
+
+	// 解析像素資訊，設定人物名字和屬性值
+	bounds := img.Bounds()
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			r, g, b, _ := img.At(x, y).RGBA()
+			switch {
+			case x < bounds.Max.X/2 && y < bounds.Max.Y/2:
+				data.Name += string(r>>8) + string(g>>8) + string(b>>8)
+			case x >= bounds.Max.X/2 && y < bounds.Max.Y/2:
+				data.Stats.HP += int(r>>8) + int(g>>8) + int(b>>8)
+			case x < bounds.Max.X/2 && y >= bounds.Max.Y/2:
+				data.Stats.MP += int(r>>8) + int(g>>8) + int(b>>8)
+			case x >= bounds.Max.X/2 && y >= bounds.Max.Y/2:
+				data.Stats.STR += int(r >> 8)
+				data.Stats.INT += int(g >> 8)
+				data.Stats.LUC += int(b >> 8)
+			}
+		}
+	}
+
+	return data, nil
+}
+
+func fight(player1 *Player, player2 *Player) Player {
 	fmt.Printf("%s vs. %s!\n", player1.Name, player2.Name)
 
 	// 隨機決定哪個玩家先攻
 	rand.Seed(time.Now().UnixNano())
-	players := []Player{player1, player2}
+	players := []Player{*player1, *player2}
 	attackerIndex := rand.Intn(2)
 	attacker := players[attackerIndex]
 	defender := players[(attackerIndex+1)%2]
